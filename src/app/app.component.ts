057615ca1d2235b02b0e507f19ee9b91dec92d0e
Ayyaz <contact@ayyaztech.com>
2024-10-09 17:42:18 +0500

    feat: implement lazy loading strategies and update routing
    - Add lazy loading for Customers and Orders modules
    - Implement lazy loading of components using ViewContainerRef
    - Add lazy loading of components using ngComponentOutlet
    - Set up lazy loading of component with RouterLink
    - Update AppRoutingModule with new lazy-loaded routes
    - Clean up AppComponent template and add new lazy loading examples
    - Update AppComponent to support lazy loading methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'customer-app';
+  viewContainerRef = inject(ViewContainerRef);
+  advancedSettings: any;
+
+  async loadComponentWithContainerRef() {
+    const { LazyLoadedComponent } = await import(
+      './lazy-loaded/lazy-loaded.component'
+    );
+
+    this.viewContainerRef.createComponent(LazyLoadedComponent)
+  }
+
+  async loadComponent() {
+    this.advancedSettings = (await import('./lazy-loaded/lazy-loaded.component')).LazyLoadedComponent
+  }
 }
